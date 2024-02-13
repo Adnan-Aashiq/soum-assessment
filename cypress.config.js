14685b99ac5f434819ac971c7ca878af8973233f
Adnan-Aashiq <adnanaashiq457@gmail.com>
2024-02-13 17:45:50 +0500

    final commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,21 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+  reporter: 'cypress-mochawesome-reporter',
   viewportWidth: 1920,
   viewportHeight: 1080,
   "chromeWebSecurity": false,
-  env: {
-  },
   e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
     baseUrl: 'https://www.saucedemo.com/',
   },
 });
